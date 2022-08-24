55c1d0d6779da0100a6c114b6d2c6800a6299243
rodrigoArceUnrn <106389494+rodrigoArceUnrn@users.noreply.github.com>
2022-08-24 19:46:19 -0300

    first commit

---
+++ src/main/java/ar/edu/unrn/shoppingservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ar.edu.unrn.shoppingservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShoppingServiceApplication.class);
+	}
+
+}
---
+++ src/test/java/ar/edu/unrn/shoppingservice/ShoppingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unrn.shoppingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
